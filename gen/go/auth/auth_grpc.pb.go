07ae3a4f76bbdfc4f6daa69ae82864a573b92b3a
khasan <requiem19990@gmail.com>
2025-08-16 16:06:23 +0400

    fix

--- gen/go/auth/auth_grpc.pb.go
+++ gen/go/auth/auth_grpc.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v6.30.2
-// source: sso/sso.proto
+// source: auth/auth.proto
 
-package ssov1
+package driveauth
 
 import (
 	context "context"
@@ -193,5 +193,5 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "sso/sso.proto",
+	Metadata: "auth/auth.proto",
 }
